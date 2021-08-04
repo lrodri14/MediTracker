5f1a905ca89b1b72a01352f02b811967bb373bb1
Luis Adolfo Rodriguez Lopez <adolfoluis.rl@gmail.com>
2021-08-03 21:12:23 -0600

    Password Reset Pages styles and JS has been added. Pretty presentation now displayed.

--- accounts/static/accounts/js/password-reset-confirm.js
+++ accounts/static/accounts/js/password-reset-confirm.js
@@ -11,25 +11,12 @@ let body = document.querySelector('body')
 /*////////////////////////////////////////////////////// Event Listeners /////////////////////////////////////////////*/
 
 // Body Event Listeners
-// Body Input Events
-body.addEventListener('input', (e) => {
-    /* This event will be fired every time the target is newPassword1 or newPassword2, this event will check if the
-       two fields are not empty, if the condition is fulfilled, the reset button will have the button-fadeIn class added,
-       if not it won't be added or it will be removed.*/
-    if (e.target === newPassword1 || e.target === newPassword2){
-        if (newPassword1.value.length > 0 && newPassword2.value.length > 0){
-            reset.classList.add('submit-button--fade-in')
-        }else{
-            reset.classList.remove('submit-button--fade-in')
-        }
-    }
-})
 
 // Body Mouseover Events
 body.addEventListener('mouseover', (e) => {
     // This event will be fired every time the target is the reset button and it will have the button-hover class added.
     if (e.target === reset){
-        reset.classList.add('submit-button--hover')
+        reset.classList.add('submit-button--active')
     }
 })
 
@@ -37,6 +24,6 @@ body.addEventListener('mouseover', (e) => {
 body.addEventListener('mouseout', (e) => {
     // This event will be fired every time the target is the reset button and it will have the button-hover class removed.
     if (e.target === reset){
-        reset.classList.remove('submit-button--hover')
+        reset.classList.remove('submit-button--active')
     }
 })
